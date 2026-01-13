426682b827bd42619da9ff64185dff8940777add
IlseHahn22 <cc241064@fhstp.ac.at>
2026-01-13 18:14:28 +0100

    Add moodRepository to MyBuddyApplication

--- code/app/src/main/java/com/example/mybuddy/MyBuddyApplication.kt
+++ code/app/src/main/java/com/example/mybuddy/MyBuddyApplication.kt
@@ -2,10 +2,12 @@ package com.example.mybuddy
 
 import android.app.Application
 import com.example.mybuddy.data.repository.BuddyRepository
+import com.example.mybuddy.data.repository.MoodRepository
 import com.example.mybuddy.db.AppDatabase
 
 class MyBuddyApplication : Application() {
 
     val database by lazy { AppDatabase.getDatabase(this) }
     val buddyRepository by lazy { BuddyRepository(database.buddyProfileDao()) }
+    val moodRepository by lazy { MoodRepository(database.moodDao()) }
 }
